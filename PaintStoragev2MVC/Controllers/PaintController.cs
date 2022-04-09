c7829a8c7b5a05b57dd8294709767cb1eb6f3f1e
Kristijan Matić <kristijan.matic@hotmail.com>
2022-04-10 01:02:00 +0200

    Add project files.

---
+++ PaintStoragev2MVC/Controllers/PaintController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using PaintStoragev2MVC.Models;
+
+namespace PaintStoragev2MVC.Controllers
+{
+    public class PaintController : Controller
+    {
+        // GET: Paint
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult GetData()
+        {
+            using (PaintStorageDBEntities db = new PaintStorageDBEntities())
+            {
+                List<PaintDB> paintList = db.PaintDB.ToList<PaintDB>();
+                return Json(new { data = paintList }, JsonRequestBehavior.AllowGet);
+            }
+        }
+    }
+}
---
+++ PaintStoragev2MVC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="PaintStoragev2MVC.MvcApplication" Language="C#" %>
